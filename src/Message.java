d8ed746d5aebcd75c2717cfaf1b3a6a5cca923dd
Sharvil Kekre <sharvilkekre@gmail.com>
2018-07-27 14:49:50 -0700

    Create Message.java

---
+++ src/Message.java
@@ -0,0 +1,15 @@
+package com.example.sslserver;
+
+public class Message {
+	private int did;
+
+	public Message() {}
+	
+	public int getDid() {
+		return did;
+	}
+
+	public void setDid(int did) {
+		this.did = did;
+	}
+}
