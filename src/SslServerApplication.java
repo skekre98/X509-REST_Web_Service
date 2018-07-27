2ad48e3320688727226c756dfac0acde01b041f6
Sharvil Kekre <sharvilkekre@gmail.com>
2018-07-27 14:25:24 -0700

    Update SslServerApplication.java

--- src/SslServerApplication.java
+++ src/SslServerApplication.java
@@ -12,3 +12,34 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+@SpringBootApplication
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SslServerApplication extends WebSecurityConfigurerAdapter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SslServerApplication.class, args);
+	}
+	
+	@Override
+    protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable(); //NOT SECURE, LATER SHOULD ACTUALLY INCLUDE CSRF TOKEN IN JSON REQUEST
+        http.authorizeRequests().anyRequest().authenticated().and().x509().subjectPrincipalRegex("CN=(.*?)(?:,|$)").userDetailsService(userDetailsService());
+    }
+	
+	@Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                if (username.equals("cid")) {
+                    return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+                }
+                throw new UsernameNotFoundException("User not found!");
+            }
+        };
+    }
+}
+
